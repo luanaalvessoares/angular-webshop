b2d34b975247feb67ba9e13419cd8bad59fed6ad
Luana Alves <luanaalvessoares_@outlook.com>
2023-06-26 14:05:33 -0300

    feat: create header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,12 @@ import { MatToolbarModule } from '@angular/material/toolbar'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
