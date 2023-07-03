3c04388e55cee1faf253b33664eb1c172a3c991b
Luana Alves <luanaalvessoares_@outlook.com>
2023-07-03 14:35:11 -0300

    feat: quantity of items to be shown, products header component

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
-  templateUrl: './products-header.component.html',
-  styles: [
-  ]
+  templateUrl: './products-header.component.html'
 })
 export class ProductsHeaderComponent {
+  sort = 'desc';
+  itemsShowCount = 12;
 
+  constructor() {}
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
+
+  onItemsUpdated(count: number): void {
+    this.itemsShowCount = count;
+  }
 }
+
